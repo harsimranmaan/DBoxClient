d5c22b7c1ae258441bc340158862a3bf064a87c5
Kuntal Joshi <kuntalce@gmail.com>
2013-07-03 12:50:33 -0700

    bootstrap

---
+++ DBoxClient/src/dboxclient/DBoxClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dboxclient;
+
+/**
+ *
+ * @author Kuntal
+ */
+public class DBoxClient
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
